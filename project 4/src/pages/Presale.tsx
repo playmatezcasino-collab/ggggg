e47b365eedd6669458aae901fdea4e0ea160066b
playmatezcasino-collab <playmatezcasino@gmail.com>
2025-09-10 23:37:10 +0100

    Update Presale.tsx

--- project 4/src/pages/Presale.tsx
+++ project 4/src/pages/Presale.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PresaleHero } from '../components/presale/PresaleHero';
 import { TrustBadges } from '../components/presale/TrustBadges';
 import { ProgressStrip } from '../components/presale/ProgressStrip';
@@ -9,6 +9,29 @@ import { FAQ } from '../components/presale/FAQ';
 import { LegalRisk } from '../components/presale/LegalRisk';
 
 export const Presale: React.FC = () => {
+  useEffect(() => {
+    // Create the Coinremitter widget script
+    const crPresaleWidget = document.createElement('script');
+    crPresaleWidget.id = 'coinremitter-presale-widget';
+    crPresaleWidget.type = 'text/javascript';
+    crPresaleWidget.src = 'https://cdn.coinremitter.com/widget/presale/MFN5nM0p09/checkout.js';
+    crPresaleWidget.charset = 'UTF-8';
+    crPresaleWidget.setAttribute('crossorigin', '*');
+
+    // Append to the container
+    const container = document.getElementById('coinremitter-widget-container');
+    if (container) {
+      container.appendChild(crPresaleWidget);
+    }
+
+    // Cleanup on unmount
+    return () => {
+      if (container && crPresaleWidget.parentNode) {
+        container.removeChild(crPresaleWidget);
+      }
+    };
+  }, []);
+
   return (
     <div className="min-h-screen section-with-top-shade">
       <div className="container mx-auto px-4 py-20">
@@ -24,6 +47,9 @@ export const Presale: React.FC = () => {
 
         {/* Progress Strip */}
         <ProgressStrip />
+
+        {/* Coinremitter Widget */}
+        <div id="coinremitter-widget-container" className="my-8"></div>
 
         {/* Benefits */}
         <PresaleBenefits />
